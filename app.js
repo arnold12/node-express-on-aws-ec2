d70737fa3f13f9c9e5297389fc31ef8ca646ebb1
Arnold Machado <machadoarnold@gmail.com>
2022-02-06 16:58:11 +0530

    updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hi How are you?</h1>');
+  res.send('<h1>Hi How are you? How you doing?</h1>');
 });
 
 app.listen(PORT, () => {
